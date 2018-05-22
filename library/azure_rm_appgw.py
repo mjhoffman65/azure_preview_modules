b4ce11a8d324faeea75609483f64995eae375ae8
zikalino <zikalino@microsoft.com>
2018-05-22 10:45:51 +0800

    fix syntax

--- library/azure_rm_appgw.py
+++ library/azure_rm_appgw.py
@@ -10,7 +10,7 @@ __metaclass__ = type
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
-                    'supported_by': 'community'}dump
+                    'supported_by': 'community'}
 
 
 DOCUMENTATION = '''
